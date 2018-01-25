41c09a47b10e01c7374ac11f2eb85acd12190175
smcgee2 <smcgee@esriuk.com>
2018-01-25 13:13:56 +0000

    Adding click

--- code/assets/js/scene.js
+++ code/assets/js/scene.js
@@ -40,6 +40,30 @@
         }
       });
 
+        
+        view.on("pointer-down", function(event){
+        view.hitTest(event)
+          .then(function(response){
+            spawnObject(response.results[0].mapPoint.longitude,response.results[0].mapPoint.latitude)
+
+            if(response.results[0].graphic != null){
+              //And if edit mode is enabled. 
+               objectHighlight()
+            }
+          })
+        });
+
+
+        function objectHighlight(){
+          console.log("object is highlighted")
+        }
+
+        function spawnObject(long, lat){
+          console.log("object spawn at " + long + ", " + lat)
+        }    
+        
+        
+        
       /********************************************************************
        * Add layer containing street furniture features: benches, street lamps
        ********************************************************************/
