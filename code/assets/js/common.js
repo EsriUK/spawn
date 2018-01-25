2955ac45abdc43b756cdec6305d4ea5be8369ea3
Richard Stevens <rstevens@esriuk.com>
2018-01-25 15:03:08 +0000

    Carousel update to build from local JSON

--- code/assets/js/common.js
+++ code/assets/js/common.js
@@ -1,19 +1,34 @@
-var $owl = $('.owl-carousel');
+$(document).ready(function() {
+ 
+	$("#owl-demo").owlCarousel({
+		jsonPath : 'assets/data/testdata.json',
+		jsonSuccess : customDataSuccess,
+		items : 10, //10 items above 1000px browser width
+		itemsDesktop : [1000,5], //5 items between 1000px and 901px
+		itemsDesktopSmall : [900,3], // betweem 900px and 601px
+		itemsTablet: [600,2], //2 items between 600 and 0
+		itemsMobile : false,
+		pagination: false,
+		rewindNav: false
+	});
+ 
+	function customDataSuccess(data){
+		var content = "";
+		for(var i in data["items"]){
+			var img = data["items"][i].src;
+			var alt = data["items"][i].alt;
+			content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+		}
+		$("#owl-demo").html(content);
+	}
 
-$owl.children().each( function( index ) {
-  $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
-});
-
-$owl.owlCarousel({
-	center: true,
-    loop: true,
-    margin: 15,
-    nav: false,
-	dots: false,
-	items: 18
-});
-
-$(document).on('click', '.owl-item > div', function() {
-	console.log($(this)[0].childNodes["0"].title);
-	$owl.trigger('to.owl.carousel', $(this).data('position') );
+	$(document).on('click', '.owl-item', function(){
+		var $this = $(this);
+		if($this.hasClass('clicked')){
+			$this.removeClass('clicked');
+		} else{
+			$this.addClass('clicked');
+		}
+	});
+ 
 });
