1673d91dd890958e22b8ce828994670756280730
smcgee2 <smcgee@esriuk.com>
2018-01-25 13:57:32 +0000

    Spawned spawn

--- code/assets/js/scene.js
+++ code/assets/js/scene.js
@@ -11,7 +11,6 @@
     ], function(WebScene, SceneView, FeatureLayer, SceneLayer, Query) {
 
         
-        console.log("am I loaded?")
       // Create the web scene
       var map = new WebScene({
         basemap: "satellite",
@@ -40,46 +39,8 @@
         }
       });
 
-        var featureArray = []
+      var featureArray = []
 
-        
-        view.on("pointer-down", function(event){
-        view.hitTest(event)
-          .then(function(response){
-            spawnObject(response.results[0].mapPoint.longitude,response.results[0].mapPoint.latitude)
-
-            if(response.results[0].graphic != null){
-              //And if edit mode is enabled. 
-               objectHighlight()
-            }
-          })
-        });
-
-
-        function objectHighlight(){
-          console.log("object is highlighted")
-        }
-
-        function spawnObject(long, lat){
-        console.log("object spawn at " + long + ", " + lat)
-        
-        featureArray.push(
-            { 
-               geometry: {
-                 type: "point",
-                 longitude: long,
-                 latitude: lat
-               },
-               attributes: {
-                 ROTATION: "30",
-                 CATEGORY: "test",
-                 SIZE: "10"
-               }
-             })
-            
-            console.log(featureArray)
-        }    
-        
         
         
       /********************************************************************
