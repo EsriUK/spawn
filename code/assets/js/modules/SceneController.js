6f822526946eb2fb052fb87c1306a3ae513a552b
Richard Mumford <rmumford@esriuk.com>
2018-01-25 18:52:21 +0000

    Tidied up the Scene Controller

--- code/assets/js/modules/SceneController.js
+++ code/assets/js/modules/SceneController.js
@@ -32,165 +32,18 @@ define([
             
             environment: {
                 lighting: {
-                date: new Date("June 15, 2015 16:00:00 EDT"),
+                date: new Date("June 15, 2015 21:00:00 GMT"),
                 directShadowsEnabled: true,
                 ambientOcclusionEnabled: true
                 }
             }
         });
-        //var mapWait = $.Deferred();
-        //mapWait.resolve();
-        //return mapWait.promise();
-   ///////////////////////////////////////////
-        
-    //   var featureArray = []
-    //   var clickCount = 0
-    //   var cachedY
-    //   var cachedX
-    //   var current
- 
-        
-    // that.view.on("click", function(event){
-    //     cachedY = event.y
-    //     cachedX = event.x
-    //     console.log(cachedY + ", " + cachedX)
-        
-    //     clickCount ++
-        
-    //      if(clickCount === 2){
-    //         console.log("reset states")
-    //        // cachedY = []
-    //        // cachedX = []
-    //         clickCount = 0;
-    //     }
-        
-    //     that.view.hitTest(event)
-
-    //       .then(function(response){
-    //         spawnObject(response.results[0].mapPoint.longitude,response.results[0].mapPoint.latitude,clickCount)
-                
-    //         if(response.results[0].graphic != null){
-    //           //And if edit mode is enabled. 
-    //            objectHighlight()
-    //         }
-    //       })
-    //     });
-
-        // function objectHighlight(){
-        //   console.log("object is highlighted")
-        // }
-        
-        
-        
-        ///////////////////////////////////////
-        
-    // function spawnObject(long, lat, clickCount){
-
-
-        
-    //     $("body").mousemove(function(e) {
-    //             featureLayerScaler(e)
-    //     })
-    
-    //     console.log(clickCount)
-            
-    //     if(clickCount === 1){
-            
-    //         console.log("object spawn at " + long + ", " + lat)
-    //         //spawning object
-    //         var temp =  { 
-    //                geometry: {
-    //                  type: "point",
-    //                  longitude: long,
-    //                  latitude: lat
-    //                },
-    //                attributes: {
-    //                  ROTATION: "0",
-    //                  CATEGORY: "test",
-    //                  SIZE: "1"
-    //                }
-    //          } 
-            
-           
-    //         console.log(temp)
-    //     } 
-   
-        
-    //     if(clickCount === 0){
-    //         console.log("save and push feature?")
-        
-    //         console.log(featureArray)
-            
-              
-    //          $("body").off(mousemove())
-            
-    //         featureArray.push(
-    //             { 
-    //                geometry: {
-    //                  type: "point",
-    //                  longitude: long,
-    //                  latitude: lat
-    //                },
-    //                attributes: {
-    //                  ROTATION: "30",
-    //                  CATEGORY: "test",
-    //                  SIZE: "10"
-    //                }
-    //          }) 
-            
-    //     }
-           
-    //     function featureLayerScaler(e){
-    //                 console.log("size = " + Math.abs((cachedY - e.pageY)/10))
-    //                 console.log("rotation = " + ((cachedX - e.pageX)/2))
-    //     }
-      
-        
-    //     }  
-        
-        ////////////////////////////////////////
-
-        function createFeatureLayer() {
-            var fields = [
-                {
-                  name: "ObjectID",
-                  alias: "ObjectID",
-                  type: "oid"
-                }, {
-                  name: "SIZE",
-                  alias: "size",
-                  type: "string"
-                }, {
-                  name: "ROTATION",
-                  alias: "rotation",
-                  type: "string"
-                }, {
-                  name: "CATEGORY",
-                  alias: "category",
-                  type: "string"
-            }];
-
-            lyr = new FeatureLayer({
-                source: featureArray, // autocast as an array of esri/Graphic
-                // create an instance of esri/layers/support/Field for each field object
-                fields: fields, // This is required when creating a layer from Graphics
-                objectIdField: "ObjectID", // This must be defined when creating a layer from Graphics
-                //renderer: quakesRenderer, // set the visualization on the layer
-                spatialReference: {
-                  wkid: 4326
-                },
-                geometryType: "point", // Must be set when creating a layer from Graphics
-            });
-        }
-        
     }
 
     SceneController.prototype.getView = function(){
         return this.view;
     }
 
-
-    //Stuff to make public
     return {
         SceneController: SceneController
         
